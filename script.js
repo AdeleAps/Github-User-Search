41d7e5ff3b33a9fae82d76db00908969b6e4dcd4
“AdeleAps” <adelea@inbox.lv>
2022-03-06 17:57:12 +0200

    Cleaned up console errors.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// saves light/dark mode preference
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  let background = window.localStorage.getItem("background");
+  if(background) {
+    document.body.className += background;
+  }
+});
+
 // light and dark mode
 
 function toggleMode(event, mode) {
@@ -10,12 +19,11 @@ function toggleMode(event, mode) {
     document.body.classList.toggle("dark-theme");
     
     if (document.body.classList.contains("dark-theme")) {
-      localStorage.setItem("background", "dark-theme");
+    window.localStorage.setItem("background", "dark-theme");
       } else {
         localStorage.setItem("background", "");
-      }
+    }
 }
-
 // GitHub API
 
 const gitHubForm = document.getElementById('gitHubForm');
@@ -29,8 +37,19 @@ gitHubForm.addEventListener('submit', (e) => {
   let input = document.querySelectorAll(".input");
   for (i = 0; i < input.length; i++) {
     input[i].textContent = "";
-  }
-})
+  document.getElementById("content").style.display = "block";
+  }}
+)
+
+
+function infoError(name) {
+  name.textContent = "Not Available";
+  name.parentElement.style.opacity = "0.5";
+}
+
+function infoSuccess(name) {
+  name.parentElement.style.opacity = "1";
+}
 
 // display API data
 
@@ -38,11 +57,16 @@ function requestUser(username){
   const xhr = new XMLHttpRequest();
   const url = `https://api.github.com/users/${username}`;
   xhr.open('GET', url, true);
+
+  xhr.onerror = function() {
+  alert("Invalid value");
+  }
+
   xhr.onload = function() {
   const data = JSON.parse(this.response);
-  console.log(data);
-
+  if ("login" in data) {
   let username = document.getElementById('username');
+
   if (!data.name) {
     username.textContent = data.login;
   } else {
@@ -82,42 +106,46 @@ function requestUser(username){
   
   let location = document.getElementById('location');
   if (!data.location) {
-    location.textContent = "Not Available";
-    location.parentElement.style.opacity = "0.5";
+  infoError(location);
   } else {
   location.textContent = data.location;
-  location.parentElement.style.opacity = "1";
+  infoSuccess(location);
 }
 
   let twitterInfo = document.getElementById('twitter-info');
   if (!data.twitter_username) {
-    twitterInfo.textContent = "Not Available";
-    twitterInfo.parentElement.style.opacity = "0.5";
+    infoError(twitterInfo);
   } else {
     twitterInfo.textContent = data.twitter_username;
-    twitterInfo.parentElement.style.opacity = "1";
+    infoSuccess(twitterInfo);
 }
 
   let website = document.getElementById('website');
   if (!data.blog) {
-    website.textContent = "Not Available";
-    website.parentElement.style.opacity = "0.5";
+    infoError(website);
   } else {
     website.textContent = data.blog;
-    website.parentElement.style.opacity = "1";
+    infoSuccess(website);
 }
 
   let company = document.getElementById('company');
   if (!data.company) {
-    company.textContent = "Not Available";
-    company.parentElement.style.opacity = "0.5";
+    infoError(company);
   } else {
     company.textContent = data.company;
-    company.parentElement.style.opacity = "1";
+    infoSuccess(company);
   }
+}
+else {
+  document.getElementById("usernameInput").value = "";
+  document.getElementById("usernameInput").setAttribute("placeholder", "Invalid Username!");
+  document.getElementById("content").style.display = "none";
+} 
 }
 xhr.send();
 }
+
+
 
 // page refresh when clicking the title
 
@@ -125,16 +153,6 @@ function refreshPage() {
   window.location.reload();
   document.getElementById("usernameInput").value = "";
 }
-
-// saves light/dark mode preference
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  let background = localStorage.getItem("background");
-  if(background) {
-    document.body.className += background;
-  }
-});
-
 
 
 
